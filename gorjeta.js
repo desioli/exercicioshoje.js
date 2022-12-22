9ba061288b36ebd8f78e3fe21a636956d45bfd59
desioli <ingdee@icloud.com>
2022-12-21 23:48:39 -0300

    labaxurias

---
+++ gorjeta.js
@@ -0,0 +1,6 @@
+function calculatorOfTips(dinner, waiter) {
+    return dinner + waiter * tips;
+}
+    let tips = 0.1;
+    let totalDinner = calculatorOfTips(80.00, 80.00)
+    console.log(totalDinner)
